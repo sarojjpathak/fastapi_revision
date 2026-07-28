bf58f2e933be50c2649d59442bacfe1699f763e6
sarojjpathk <pathaksaroj695@gmail.com>
2026-07-28 12:46:08 +0545

    Add CRUD operations for Todo API using FastAPI

--- crud.py
+++ crud.py
@@ -1,36 +1,57 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-
 app = FastAPI()
-
-#todo app
 todo = []
-
 class Todo(BaseModel):
-    id:int
-    todo:str
-    status:bool
-@app.post("/todo")
-def create_todo(todos:Todo):
-    todo.append(todos)
-    return {"message":"todo added","ToDo":todos}
-@app.get("/todo")
+    id: int = 1
+    todo: str = "learn fast API"
+    status: bool
+@app.get("/")
 def home():
+    return {"list": todo}
+@app.post("/todo")
+def create_todo(todos: Todo):
+    todo.append(todos)
+    return {
+        "message": "Todo added successfully",
+        "todo": todos
+    }
 
-    return {"message":"this are the list of thing you need to complete" ,"list":todo}
 
-
-#know we try to get the data based on id
+# Get todo by ID
 @app.get("/todo/{id}")
-def filter_data(id:int):
-   for todoo in todo:
-      if todoo["id"] == id:
-         return todoo
-      
-   return{"message":"invalid id"}
-   
+def filter_data(id: int):
+    for todoo in todo:
+        if todoo.id == id:
+            return {"data": todoo}
+
+    return {"message": "Invalid ID"}
 
 
+# Update existing todo
+@app.put("/update/{userid}")
+def update(userid: int, updatedtodo: Todo):
+    for index, todoo in enumerate(todo):
+        if todoo.id == userid:
+            todo[index] = updatedtodo
+            return {
+                "message": "Updated successfully",
+                "todo": updatedtodo
+            }
 
-#updating existing data using put or patch
+    return {"message": "Todo not found"}
+
+
+# Delete todo
+@app.delete("/delete/{id_to_delete}")
+def delete(id_to_delete: int):
+    for index, todoo in enumerate(todo):
+        if todoo.id == id_to_delete:
+            del todo[index]
+            return {
+                "message": "Deleted successfully",
+                "list": todo
+            }
+
+    return {"message": "Todo not found"}
